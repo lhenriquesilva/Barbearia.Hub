10d4c9c5f06db619db0f3a66cf1c1ac584fba034
Luiz henrique <lhenriquesilva171@gmail.com>
2025-11-16 02:18:11 -0300

    agendamento
    agendamento atualizado

--- INICIALefeito.js
+++ INICIALefeito.js
@@ -31,3 +31,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener("scroll", animarContagem);
 });
+
+const btns = document.querySelectorAll(".btn-agendar");
+const modal = document.getElementById("modal-agendamento");
+const fechar = document.getElementById("fecharModal");
+const form = document.getElementById("formAgendamento");
+
+// Abrir o modal
+btns.forEach(btn => {
+    btn.addEventListener("click", function (e) {
+        e.preventDefault();
+        modal.style.display = "flex";
+    });
+});
+
+// Fechar no X
+fechar.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// Fechar ao enviar o formulário
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Agendamento finalizado com sucesso!");
+    modal.style.display = "none";
+    form.reset();
+});
+
+// Fechar clicando fora do modal
+window.addEventListener("click", function (e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
